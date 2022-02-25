275562e1293a13390df6b55d69e86594f546d964
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-02-25 18:46:17 +0100

    adding a sinlge linebreak

--- pages/index.js
+++ pages/index.js
@@ -229,7 +229,6 @@ export default function Home() {
         );
         listFilteredByCreators.push(entriesByPerson);
       }
-
       filteredResults = _.flatten(listFilteredByCreators);
     }
 
