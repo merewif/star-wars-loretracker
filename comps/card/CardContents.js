a0a90f92f0b787db481ca9f2e190c274f0b90b3d
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-03-27 04:28:40 +0200

    render for numbers added

--- comps/card/CardContents.js
+++ comps/card/CardContents.js
@@ -2,22 +2,35 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 import LinksContainer from "./LinksContainer";
 import moment from "moment";
+import Image from "next/image";
 
 export default function CardContents({ i2, currentKey, currentValue }) {
   return (
     <div key={"2" + i2}>
       {currentKey === "coverImage" ? (
         <div className={styles.coverImageContainer}>
-          <img className={styles.coverImage} src={currentValue} />
+          <img
+            className={styles.coverImage}
+            src={currentValue}
+            alt="Cover Image"
+          />
         </div>
       ) : currentKey === "links" ? (
         <LinksContainer currentValue={currentValue} />
       ) : currentKey === "canonicity" ? null : (
         <h2>{currentKey.replace(/([A-Z])/g, " $1")}:</h2>
       )}
+
       {typeof currentValue === "string" && currentKey !== "coverImage" ? (
         <p>{currentValue}</p>
       ) : null}
+
+      {typeof currentValue === "number" &&
+      currentKey !== "timeline" &&
+      currentKey !== "coverImage" ? (
+        <p>{currentValue}</p>
+      ) : null}
+
       {currentKey === "releaseDate" && moment(currentValue).isValid() ? (
         <p>
           {moment(currentValue)._d.toDateString().split(" ").slice(1).join(" ")}
@@ -30,7 +43,6 @@ export default function CardContents({ i2, currentKey, currentValue }) {
         <p>{Math.abs(currentValue).toLocaleString("en")} BBY</p>
       ) : null}
       {currentKey === "timeline" && currentValue === "" ? <p>N/A</p> : null}
-
       {currentKey === "canonicity" ? (
         currentValue ? (
           <div className={styles.canonDiv}>
