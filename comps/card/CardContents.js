79263cf01ec31d22b9f8c25486dfca97aa38213f
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-03-28 22:56:22 +0200

    reverting change

--- comps/card/CardContents.js
+++ comps/card/CardContents.js
@@ -37,10 +37,10 @@ export default function CardContents({ i2, currentKey, currentValue }) {
         </p>
       ) : null}
       {currentKey === "timeline" && currentValue > 0 ? (
-        <p>{Math.abs(currentValue)} ABY</p>
+        <p>{Math.abs(currentValue).toLocaleString("en")} ABY</p>
       ) : null}
       {currentKey === "timeline" && currentValue <= 0 ? (
-        <p>{Math.abs(currentValue)} BBY</p>
+        <p>{Math.abs(currentValue).toLocaleString("en")} BBY</p>
       ) : null}
       {currentKey === "timeline" && currentValue === "" ? <p>N/A</p> : null}
       {currentKey === "canonicity" ? (
