054b72fb6248ef2295ac8ba0fe5b74d02bb0e8ee
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-03-28 23:40:34 +0200

    testing timeline parsing

--- pages/index.js
+++ pages/index.js
@@ -147,12 +147,13 @@ export default function Home() {
       currentBook.releaseDate = moment(book["Release Date"]);
       currentBook.category = book["Category"];
       currentBook.links = {};
-      const doesTimelineIncludeTwoDates =
+
+      const timelineIncludesTwoDates =
         book["Timeline"].includes("-") ||
         book["Timeline"].includes("/") ||
         book["Timeline"].includes("—");
 
-      if (doesTimelineIncludeTwoDates) {
+      if (timelineIncludesTwoDates) {
         const fullDate = book["Timeline"]
           .replace(/\s|,/g, "")
           .replace(/\/|—/, "-");
@@ -160,9 +161,6 @@ export default function Home() {
         let eras = fullDate.match(/([A-Z]{3})/g);
         if (eras.length === 1) eras[1] = eras[0];
         let dates = fullDate.match(/[^\d]*(\d+)[^\d]*\-[^\d]*(\d+)[^\d]*/);
-        // let dates = fullDate.match(
-        //   /[^\d]*(\d+)[^\d]*[\-\/\—][^\d]*(\d+)[^\d]*/
-        // );
         dates.shift();
 
         if (eras[1] === "BBY") currentBook.timeline = Number(`-${dates[1]}`);
@@ -172,37 +170,15 @@ export default function Home() {
           console.log(book["Timeline"], fullDate, currentBook.timeline);
       }
 
-      if (book["Timeline"].endsWith("BBY") && !doesTimelineIncludeTwoDates) {
+      if (book["Timeline"].endsWith("BBY") && !timelineIncludesTwoDates) {
         currentBook.timeline = Number(
           `-${book["Timeline"].replace(/[^0-9]/g, "")}`
         );
       }
 
-      if (book["Timeline"].endsWith("ABY") && !doesTimelineIncludeTwoDates) {
+      if (book["Timeline"].endsWith("ABY") && !timelineIncludesTwoDates) {
         currentBook.timeline = Number(book["Timeline"].replace(/[^0-9]/g, ""));
       }
-
-      // Last working version (safekeeping)
-      // if (book["Timeline"].includes("-")) {
-      //   const fullDate = book["Timeline"].replace(/\s|,/g, "");
-      //   let eras = fullDate.match(/([A-Z]{3})/g);
-      //   if (eras.length === 1) eras[1] = eras[0];
-      //   let dates = fullDate.match(/[^\d]*(\d+)[^\d]*\-[^\d]*(\d+)[^\d]*/);
-      //   dates.shift();
-
-      //   if (eras[1] === "BBY") currentBook.timeline = Number(`-${dates[1]}`);
-      //   if (eras[1] === "ABY") currentBook.timeline = Number(`${dates[1]}`);
-      // }
-
-      // if (book["Timeline"].endsWith("BBY") && !book["Timeline"].includes("-")) {
-      //   currentBook.timeline = Number(
-      //     `-${book["Timeline"].replace(/[^0-9]/g, "")}`
-      //   );
-      // }
-
-      // if (book["Timeline"].endsWith("ABY") && !book["Timeline"].includes("-")) {
-      //   currentBook.timeline = Number(book["Timeline"].replace(/[^0-9]/g, ""));
-      // }
 
       if (
         currentBook.category === "Adult Novel" ||
@@ -215,7 +191,6 @@ export default function Home() {
         books.push(currentBook);
       }
     }
-    // setFetchedData(_.slice(books, 0, 51));
     setFetchedData(books);
     setFetchedGiganticData(books);
     setDefaultFetchedData(books);
