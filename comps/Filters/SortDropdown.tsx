42c62f3c7fcd530e3b0551111e6beb6a36330398
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-06 04:10:30 +0200

    switching double quotes to single quotes

--- comps/Filters/SortDropdown.tsx
+++ comps/Filters/SortDropdown.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import _ from "lodash";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import styles from "../../styles/Home.module.css";
+import React, { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import _ from 'lodash';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import styles from '../../styles/Home.module.css';
+import { SortDropdownProps } from '../../types';
 
-export default function SortDropdown({ sortBy, orderBy, moduleKeys }) {
-  const [order, setOrder] = useState("asc");
-  const [sortParameter, setSortParameter] = useState("");
+export default function SortDropdown({
+  sortBy,
+  orderBy,
+  moduleKeys,
+}: SortDropdownProps) {
+  const [order, setOrder] = useState('asc');
+  const [sortParameter, setSortParameter] = useState('');
   useEffect(() => {
     sortWithOrderParameter();
   }, [order]);
@@ -23,28 +28,28 @@ export default function SortDropdown({ sortBy, orderBy, moduleKeys }) {
   return (
     <Box
       sx={{
-        marginLeft: "auto",
-        marginTop: "auto",
-        marginBottom: "auto",
-        display: "flex",
-        alignItems: "center",
+        marginLeft: 'auto',
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        display: 'flex',
+        alignItems: 'center',
       }}
       className={styles.sortByDropdown}
     >
-      <FormControl sx={{ width: "200px" }}>
+      <FormControl sx={{ width: '200px' }}>
         <InputLabel>Sort By</InputLabel>
         <Select
           value={sortBy}
-          label="Sort By"
+          label='Sort By'
           onChange={(e) => {
             orderBy(e.target.value, order);
             setSortParameter(e.target.value);
           }}
         >
-          {_.without(moduleKeys, "coverImage", "links").map((e, i) => {
+          {_.without(moduleKeys, 'coverImage', 'links').map((e, i) => {
             return (
               <MenuItem value={e} key={e}>
-                {e.replace(/([A-Z])/g, " $1").replace(/^./, function (e) {
+                {e.replace(/([A-Z])/g, ' $1').replace(/^./, function (e) {
                   return e.toUpperCase();
                 })}
               </MenuItem>
@@ -52,15 +57,15 @@ export default function SortDropdown({ sortBy, orderBy, moduleKeys }) {
           })}
         </Select>
       </FormControl>
-      {order === "asc" ? (
+      {order === 'asc' ? (
         <ArrowUpwardIcon
-          onClick={() => setOrder("desc")}
-          sx={{ fontSize: "2rem", cursor: "pointer" }}
+          onClick={() => setOrder('desc')}
+          sx={{ fontSize: '2rem', cursor: 'pointer' }}
         />
       ) : (
         <ArrowDownwardIcon
-          onClick={() => setOrder("asc")}
-          sx={{ fontSize: "2rem", cursor: "pointer" }}
+          onClick={() => setOrder('asc')}
+          sx={{ fontSize: '2rem', cursor: 'pointer' }}
         />
       )}
     </Box>
