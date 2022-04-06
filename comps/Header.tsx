29e355580456a6984707cd5384dd016b6718d851
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-06 02:32:17 +0200

    typescript implemented for header component

--- comps/Header.tsx
+++ comps/Header.tsx
@@ -1,22 +1,29 @@
-import React, { useState, forwardRef } from "react";
-import styles from "../styles/Header.module.css";
-import logo from "../assets/logo.png";
-import Image from "next/image";
-import Dialog from "@mui/material/Dialog";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Slide from "@mui/material/Slide";
-import { style } from "@mui/system";
-import FileUploadIcon from "@mui/icons-material/FileUpload";
-import DownloadIcon from "@mui/icons-material/Download";
-import Button from "@mui/material/Button";
+import React, { useState, forwardRef } from 'react';
+import styles from '../styles/Header.module.css';
+import logo from '../assets/logo.png';
+import Image from 'next/image';
+import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import { style } from '@mui/system';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import DownloadIcon from '@mui/icons-material/Download';
+import Button from '@mui/material/Button';
+import { TransitionProps } from '@mui/material/transitions';
+import { HeaderProps } from '../types';
 
-const Transition = forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction='up' ref={ref} {...props} />;
 });
 
-const Header = ({ displayData, handleFileRead }) => {
-  const listElements = ["Movies", "Books", "Comics", "Series"];
+const Header = ({ displayData, handleFileRead }: HeaderProps) => {
+  const listElements = ['Movies', 'Books', 'Comics', 'Series'];
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -27,22 +34,23 @@ const Header = ({ displayData, handleFileRead }) => {
     setOpen(false);
   };
 
-  function uploadBackup(event) {
+  function uploadBackup(event: React.ChangeEvent<HTMLInputElement>) {
     let reader = new FileReader();
-    reader.readAsText(event.target.files[0]);
+    reader.readAsText(event.target.files![0]);
     reader.onload = handleFileRead;
   }
 
   function downloadBackup() {
-    let collection = JSON.parse(localStorage.getItem("loretracker")) ?? {};
+    let storedCollection = localStorage.getItem('loretracker') ?? '';
+    let collection = JSON.parse(storedCollection) ?? {};
     let collectionAsText = JSON.stringify(collection);
 
-    let a = document.createElement("a");
+    let a = document.createElement('a');
     a.setAttribute(
-      "href",
-      "data:text/plain;charset=utf-8," + encodeURIComponent(collectionAsText)
+      'href',
+      'data:text/plain;charset=utf-8,' + encodeURIComponent(collectionAsText)
     );
-    a.setAttribute("download", "my-star-wars-loretracker-collection.sw");
+    a.setAttribute('download', 'my-star-wars-loretracker-collection.sw');
     a.click();
   }
 
@@ -50,16 +58,16 @@ const Header = ({ displayData, handleFileRead }) => {
     <>
       <nav id={styles.navbar}>
         <div id={styles.image}>
-          <Image src={logo} alt="Logo" height={100} width={150} />
+          <Image src={logo} alt='Logo' height={100} width={150} />
         </div>
         <ul>
           {listElements.map((e, i) => {
             return (
               <li
-                onClick={(e) => displayData(e.target.id)}
+                onClick={(e) => displayData(listElements[i].toLowerCase())}
                 key={i}
                 id={listElements[i].toLowerCase()}
-                className="navbtn"
+                className='navbtn'
               >
                 {listElements[i]}
               </li>
@@ -74,15 +82,15 @@ const Header = ({ displayData, handleFileRead }) => {
         TransitionComponent={Transition}
         keepMounted
         onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-        sx={{ maxHeight: "none", maxWidth: "none" }}
+        aria-describedby='alert-dialog-slide-description'
+        sx={{ maxHeight: 'none', maxWidth: 'none' }}
       >
         <div className={styles.dialogContent}>
           <IconButton
-            aria-label="close"
+            aria-label='close'
             onClick={handleClose}
             sx={{
-              position: "absolute",
+              position: 'absolute',
               right: 8,
               top: 8,
               color: (theme) => theme.palette.grey[500],
@@ -90,26 +98,26 @@ const Header = ({ displayData, handleFileRead }) => {
           >
             <CloseIcon />
           </IconButton>
-          <Image src={logo} alt="Logo" height={200} width={300} />
+          <Image src={logo} alt='Logo' height={200} width={300} />
           <p>
             The site was developed using React and Next.js. The repository is
             available
             <a
-              href="https://github.com/merewif/star-wars-loretracker"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://github.com/merewif/star-wars-loretracker'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               here.
-            </a>{" "}
+            </a>{' '}
             For feedback and bug reports kindly use the GitHub issues feature.
             Pull requests are welcome.
           </p>
           <p>
-            The databases for the Star Wars books and comics were assembled by{" "}
+            The databases for the Star Wars books and comics were assembled by{' '}
             <a
-              href="https://youtini.com"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://youtini.com'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               the Youtini team.
             </a>
@@ -119,26 +127,26 @@ const Header = ({ displayData, handleFileRead }) => {
             stored in your browser. If you want to import or export your
             collection, use the buttons below:
           </p>
-          <div style={{ display: "grid", gap: "10px" }}>
+          <div style={{ display: 'grid', gap: '10px' }}>
             <input
               onChange={uploadBackup}
-              type="file"
+              type='file'
               hidden
-              id="upload-backup"
+              id='upload-backup'
             />
-            <label htmlFor="upload-backup">
+            <label htmlFor='upload-backup'>
               <Button
-                component="span"
-                variant="outlined"
+                component='span'
+                variant='outlined'
                 startIcon={<FileUploadIcon />}
                 sx={{
-                  color: "white",
-                  borderColor: "white",
-                  padding: "10px",
-                  minWidth: "100px",
-                  height: "2.5rem",
-                  marginBlock: "auto",
-                  width: "100%",
+                  color: 'white',
+                  borderColor: 'white',
+                  padding: '10px',
+                  minWidth: '100px',
+                  height: '2.5rem',
+                  marginBlock: 'auto',
+                  width: '100%',
                 }}
               >
                 Upload backup
@@ -146,15 +154,15 @@ const Header = ({ displayData, handleFileRead }) => {
             </label>
             <Button
               onClick={downloadBackup}
-              variant="outlined"
+              variant='outlined'
               startIcon={<DownloadIcon />}
               sx={{
-                color: "white",
-                borderColor: "white",
-                padding: "10px",
-                minWidth: "100px",
-                height: "2.5rem",
-                marginBlock: "auto",
+                color: 'white',
+                borderColor: 'white',
+                padding: '10px',
+                minWidth: '100px',
+                height: '2.5rem',
+                marginBlock: 'auto',
               }}
             >
               Download backup
@@ -165,11 +173,11 @@ const Header = ({ displayData, handleFileRead }) => {
             endorsed by, or in any way officially connected with STAR WARS,
             Lucasfilm Ltd., The Walt Disney Company, Disney Enterprises Inc., or
             any of its subsidiaries or its affiliates. The official Star Wars
-            website is available at{" "}
+            website is available at{' '}
             <a
-              href="http://www.starwars.com"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='http://www.starwars.com'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               www.starwars.com
             </a>
