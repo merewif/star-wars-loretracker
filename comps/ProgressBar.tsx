0a8a94f7522cd156a558e6c56f7aaeec2f3efa09
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-06 04:16:02 +0200

    adding type to progress bar component

--- comps/ProgressBar.tsx
+++ comps/ProgressBar.tsx
@@ -1,39 +1,40 @@
-import React, { useLayoutEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
+import React, { useLayoutEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
 import LinearProgress, {
   linearProgressClasses,
-} from "@mui/material/LinearProgress";
-import styles from "../styles/ProgressBar.module.css";
+} from '@mui/material/LinearProgress';
+import styles from '../styles/ProgressBar.module.css';
+import { ProgressBar } from '../types';
 
-export default function ProgressBar({ progressBarValue }) {
-  const [saberColor, setSaberColor] = useState("#69D8F0");
+export default function ProgressBar({ progressBarValue }: ProgressBar) {
+  const [saberColor, setSaberColor] = useState('#69D8F0');
 
   function toggleSaberColor() {
-    if (saberColor === "#69D8F0") setSaberColor("#F40001");
-    if (saberColor === "#F40001") setSaberColor("#69D8F0");
+    if (saberColor === '#69D8F0') setSaberColor('#F40001');
+    if (saberColor === '#F40001') setSaberColor('#69D8F0');
   }
   const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     height: 25,
     borderRadius: 50,
     borderTopLeftRadius: 0,
     borderBottomLeftRadius: 0,
-    margin: "auto",
-    width: "80vw",
+    margin: 'auto',
+    width: '80vw',
     boxShadow: `0px 0px 8px 8px ${saberColor}, inset 0px 0px 20px 50px ${saberColor}`,
     [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: "transparent",
+      backgroundColor: 'transparent',
     },
     [`& .${linearProgressClasses.bar}`]: {
       borderRadius: 0,
-      backgroundColor: theme.palette.mode === "light" ? "#fff" : "#fff",
+      backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#fff',
       borderTopRightRadius: 50,
       borderBottomRightRadius: 50,
     },
   }));
   return (
     <div className={styles.progressBarContainer}>
-      <img src="./imgs/hilt.png" onClick={toggleSaberColor} />
-      <BorderLinearProgress variant="determinate" value={progressBarValue} />
+      <img src='./imgs/hilt.png' onClick={toggleSaberColor} />
+      <BorderLinearProgress variant='determinate' value={progressBarValue} />
       <p>{Math.round(progressBarValue)}% FINISHED</p>
     </div>
   );
