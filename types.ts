a1df4082e743269d0b449c6262fc21c8e57cc45d
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-06 04:11:13 +0200

    switching double quotes to single quotes

--- types.ts
+++ types.ts
@@ -10,3 +10,42 @@ export interface HeaderProps {
   displayData: Function;
   handleFileRead: any;
 }
+
+export interface EntryData {
+  canonicity: boolean;
+  coverImage: string | number | undefined;
+  title: string;
+  author?: string;
+  releaseDate?: any;
+  timeline?: number | '';
+  category?: string;
+  directedBy?: string;
+  createdBy?: string;
+  yearOfRelease?: string | number;
+  era?: string;
+  seasons?: string | number;
+  episodes?: string | number;
+  links?: any;
+  '(All Books) Title + Format'?: string;
+  'Database ID'?: number;
+  'Name (Title)'?: string;
+  'Author / Writer'?: string;
+  'Release Date'?: string;
+  Timeline?: string;
+  'Cover Image URL'?: string;
+  Category?: string;
+}
+
+export interface MarkedEntries {
+  [key: string]: any;
+}
+
+export interface Keyable {
+  [key: string]: any;
+}
+
+export interface SortDropdownProps {
+  sortBy: string;
+  orderBy: Function;
+  moduleKeys: string[];
+}
