f67b712019738346da511abd0a5c25e70a4feb72
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-11 07:03:54 +0200

    Adding Google Analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
-import "../styles/globals.css";
-import React, { useEffect } from "react";
+import '../styles/globals.css';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 function StarWarsLoretracker({ Component, pageProps }) {
+  ReactGA.initialize('G-WEW3SZLHC9');
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
-    <div id="appContainer">
+    <div id='appContainer'>
       <Component {...pageProps} />
     </div>
   );
