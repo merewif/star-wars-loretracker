fc8bbee8186ebc247e94a9f0271302bdf8d98db9
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-11 23:54:13 +0200

    Initializing next-auth.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,31 +3,31 @@ import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import TwitterProvider from 'next-auth/providers/twitter';
 import RedditProvider from 'next-auth/providers/reddit';
-// pages/api/auth/[...nextauth].js
 import InstagramProvider from 'next-auth/providers/instagram';
 
 export default NextAuth({
   providers: [
     FacebookProvider({
-      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET,
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     TwitterProvider({
-      clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET,
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: '2.0',
     }),
     RedditProvider({
-      clientId: process.env.NEXT_PUBLIC_REDDIT_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_REDDIT_CLIENT_SECRET,
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET,
     }),
     InstagramProvider({
-      clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET,
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
