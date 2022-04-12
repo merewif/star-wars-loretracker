fc92260d3ebf8bac94cbbd281001643b22232ed9
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-12 09:52:28 +0200

    Changing twitter OAuth version to 2.0.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,6 +18,7 @@ export default NextAuth({
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: '2.0',
     }),
     RedditProvider({
       clientId: process.env.REDDIT_CLIENT_ID,
