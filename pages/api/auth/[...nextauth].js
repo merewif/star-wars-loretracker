725250cac8f704a11b45e42279b721ea3a0583a9
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-12 10:13:41 +0200

    Adding terms of service.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,7 +18,6 @@ export default NextAuth({
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
-      version: '1.1',
     }),
     RedditProvider({
       clientId: process.env.REDDIT_CLIENT_ID,
