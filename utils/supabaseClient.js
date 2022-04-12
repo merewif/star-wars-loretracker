2b172aa1db0c0e3f58842e21c76e3ce3eac7ad4f
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-12 15:16:04 +0200

    changing environmental variable names

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
+//
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
