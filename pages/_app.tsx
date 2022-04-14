b1823907f911df6e8016fbc8605afadae58f77ae
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2022-04-14 19:29:50 +0200

    Converting to ts file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css';
 import React from 'react';
 import ReactGA from 'react-ga';
+import { AppProps } from 'next/app';
 
-function StarWarsLoretracker({ Component, session, pageProps }) {
+function StarWarsLoretracker({ Component, pageProps }: AppProps) {
   ReactGA.initialize('G-WEW3SZLHC9');
   if (typeof window !== 'undefined') {
     ReactGA.pageview(window.location.pathname + window.location.search);
